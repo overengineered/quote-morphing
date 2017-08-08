c9d77ff5c902624bf0b468e6623fec2ec23d49fb
Juozas Kontvainis <juozas@delfi.lt>
2017-08-08 17:27:24 +0300

    Detect a change which wraps a single quote with other quotes and replace that original quote with new quotes instead

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,102 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "quote-morphing" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    vscode.workspace.onDidChangeTextDocument(event => {
+        findQuotesToMorph(event);
     });
-
-    context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+const QUOTES = ['\'', '"', '`'];
+const LOGGING_ENABLED = false;
+const log = (...args) => { if (LOGGING_ENABLED) console.log(args); }
+
+function findQuotesToMorph(event: vscode.TextDocumentChangeEvent) {
+    let wrapping = findWrappingSingleCharacterInQuotes(event);
+    if (!wrapping) {
+        return;
+    }
+
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+
+    const wrappedStart = wrapping.start.translate(0, wrapping.insertedQuote.length);
+    const wrapped = editor.document.getText(new vscode.Range(wrappedStart, wrapping.end));
+
+    if (QUOTES.indexOf(wrapped) == -1) {
+        log(`abort - wrapped '${wrapped}' is not a quote`);
+        return;
+    }
+
+    editor.edit((editBuilder) => {
+        const wrappingRange = new vscode.Range(wrapping.start,
+            wrapping.end.translate(0, wrapping.insertedQuote.length));
+        editBuilder.replace(wrappingRange, wrapping.insertedQuote);
+    });
+}
+
+function findWrappingSingleCharacterInQuotes(event: vscode.TextDocumentChangeEvent) {
+    return detectInsertionOf2QuotesAroundSingleCharacter(event);
+}
+
+function detectInsertionOf2QuotesAroundSingleCharacter(event: vscode.TextDocumentChangeEvent) {
+    if (event.contentChanges.length != 2) {
+        log(`abort - made ${event.contentChanges.length} changes`);
+        return;
+    }
+
+    const first = event.contentChanges[0];
+    const second = event.contentChanges[1];
+    if (first.text != second.text) {
+        log(`abort - inserted texts do not match`);
+        return;
+    }
+
+    if (QUOTES.indexOf(first.text) == -1) {
+        log(`abort - '${first.text}' is not a quote`);
+        return;
+    }
+
+    if (first.rangeLength != 0 || second.rangeLength != 0) {
+        log(`abort - some text was replaced`);
+        return;
+    }
+
+    if (first.range.start.line !== second.range.start.line) {
+        log(`abort - insertion in multiple lines`);
+        return;
+    }
+
+    const insertion1 = first.range.start.character;
+    const insertion2 = second.range.start.character;
+
+    let wrappedCharacterCount, start, end;
+    if (insertion1 > insertion2) {
+        wrappedCharacterCount = insertion1 - insertion2;
+        start = new vscode.Position(second.range.start.line, insertion2);
+        end = new vscode.Position(first.range.start.line, insertion1 + second.text.length);
+    } else {
+        wrappedCharacterCount = insertion2 - insertion1 - first.text.length;
+        start = new vscode.Position(first.range.start.line, insertion1);
+        end = new vscode.Position(second.range.start.line, insertion2);
+    }
+
+    if (wrappedCharacterCount != 1) {
+        log(`abort - wrapped ${wrappedCharacterCount} characters`);
+        return;
+    }
+
+    const result = {
+        insertedQuote: first.text,
+        start,
+        end
+    };
+
+    log(`single character wrapped in quotes ${JSON.stringify(result)}`);
+    return result;
 }
